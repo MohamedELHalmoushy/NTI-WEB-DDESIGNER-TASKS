3af8a3d1be4ba64a8166e4b3e153ad24c43de1d0
MohamedELHalmoushy <01228838159.z@gmail.com>
2025-09-03 13:58:39 +0300

    Add files via upload

---
+++ JAVA_SCRIPT_TASK_3/javascript/java.js
@@ -0,0 +1,21 @@
+var users = [];
+var form = document.getElementById("userForm");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var name = e.target.elements.name.value;
+    var email = e.target.elements.email.value;
+    var user = { name: name, email: email, };
+    users.push(user);
+    console.log(users);
+    form.reset();
+
+
+
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'alert alert-success position-fixed top-50 start-50 translate-middle';
+    messageDiv.innerHTML = '<strong>Success!</strong> Form submitted successfully. <br>';
+    messageDiv.innerHTML += ' <img src="images/done.png" alt="Success" style="width:300px;height:300px;">';
+    document.body.appendChild(messageDiv);
+    setTimeout(() => { messageDiv.remove(); }, 3000);
+
+});
